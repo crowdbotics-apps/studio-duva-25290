d7c2bc1654c33a6c8a5432ee97ea58ef3bbbf61a
Crowdbotics <team@crowdbotics.com>
2021-03-26 17:27:19 +0000

    Update models

--- backend/studio_duva_25290/wsgi.py
+++ backend/studio_duva_25290/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio_duva_25290.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studio_duva_25290.settings")
 
 application = get_wsgi_application()
